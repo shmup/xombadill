c5d0fb138415d895a819bad0ca3b3746b11433cd
Jared Miller <jared@smell.flowers>
2025-05-03 13:32:34 -0400

    Generate a new erlang mix

---
+++ lib/xombadill.ex
@@ -0,0 +1,18 @@
+defmodule Xombadill do
+  @moduledoc """
+  Documentation for `Xombadill`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xombadill.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xombadill_test.exs
@@ -0,0 +1,8 @@
+defmodule XombadillTest do
+  use ExUnit.Case
+  doctest Xombadill
+
+  test "greets the world" do
+    assert Xombadill.hello() == :world
+  end
+end
