9dc6b5e03b6cb9de80e102cdcb0d462f5c375855
Jared Miller <jared@smell.flowers>
2025-05-03 15:08:24 -0400

    Wire in new registry to application and client

--- lib/xombadill/application.ex
+++ lib/xombadill/application.ex
@@ -4,21 +4,44 @@ defmodule Xombadill.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Xombadill.Worker.start_link(arg)
-      # {Xombadill.Worker, arg}
-      {Xombadill.IrcClient, [
-        nick: "xombadill",
-        channels: ["#splat"]
-      ]}
+      {Xombadill.HandlerRegistry, []},
+      {Xombadill.IrcClient,
+       [
+         nick: "xombadill",
+         channels: ["#splat"]
+       ]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Xombadill.Supervisor]
-    Supervisor.start_link(children, opts)
+    result = Supervisor.start_link(children, opts)
+
+    register_default_handlers()
+
+    result
+  end
+
+  @doc """
+  Register the default handlers that should be active on startup.
+  """
+  def register_default_handlers do
+    # Register the echo handler
+    Xombadill.HandlerRegistry.register(Xombadill.Handlers.EchoHandler)
+    Logger.info("Registered default handlers")
+  end
+
+  @doc """
+  Helper function to load and register a new handler at runtime.
+  """
+  def load_handler(module) when is_atom(module) do
+    Code.ensure_loaded(module)
+
+    Xombadill.HandlerRegistry.register(module)
   end
 end
--- lib/xombadill/irc_client.ex
+++ lib/xombadill/irc_client.ex
@@ -1,11 +1,41 @@
 defmodule Xombadill.IrcClient do
   use GenServer
   require Logger
+  alias Xombadill.HandlerRegistry
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
+  @doc """
+  Send a message to a channel.
+  """
+  def say(channel, message) do
+    GenServer.cast(__MODULE__, {:say, channel, message})
+  end
+
+  @doc """
+  Join a channel.
+  """
+  def join(channel) do
+    GenServer.cast(__MODULE__, {:join, channel})
+  end
+
+  @doc """
+  Leave a channel.
+  """
+  def leave(channel) do
+    GenServer.cast(__MODULE__, {:leave, channel})
+  end
+
+  @doc """
+  Get the current state of the IRC client.
+  """
+  def get_state do
+    GenServer.call(__MODULE__, :get_state)
+  end
+
+  @impl true
   def init(opts) do
     {:ok, client} = ExIRC.start_link!()
 
@@ -23,7 +53,31 @@ defmodule Xombadill.IrcClient do
     {:ok, state}
   end
 
+  @impl true
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_cast({:say, channel, message}, state) do
+    ExIRC.Client.msg(state.client, :privmsg, channel, message)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast({:join, channel}, state) do
+    ExIRC.Client.join(state.client, channel)
+    {:noreply, %{state | channels: [channel | state.channels] |> Enum.uniq()}}
+  end
+
+  @impl true
+  def handle_cast({:leave, channel}, state) do
+    ExIRC.Client.part(state.client, channel)
+    {:noreply, %{state | channels: state.channels -- [channel]}}
+  end
+
   # handle connection messages
+  @impl true
   def handle_info({:connected, server, port}, state) do
     Logger.info("Connected to #{server}:#{port}")
     ExIRC.Client.logon(state.client, state.nick, state.nick, state.nick, state.nick)
@@ -33,9 +87,11 @@ defmodule Xombadill.IrcClient do
   # handle login success
   def handle_info(:logged_in, state) do
     Logger.info("Logged in to server")
+
     Enum.each(state.channels, fn channel ->
       ExIRC.Client.join(state.client, channel)
     end)
+
     {:noreply, state}
   end
 
@@ -63,36 +119,85 @@ defmodule Xombadill.IrcClient do
   # handle channel messages
   def handle_info({:received, msg, info, channel}, state) do
     IO.puts("#{channel} #{info.nick}: #{msg}")
+
+    # Pass to handler registry
+    HandlerRegistry.handle_message(:channel_message, %{
+      text: msg,
+      nick: info.nick,
+      user: info.user,
+      host: info.host,
+      channel: channel,
+      client: state.client
+    })
+
     {:noreply, state}
   end
 
   # handle private messages
   def handle_info({:received, msg, info}, state) do
     IO.puts("PRIVATE #{info.nick}: #{msg}")
+
+    # Pass to handler registry
+    HandlerRegistry.handle_message(:private_message, %{
+      text: msg,
+      nick: info.nick,
+      user: info.user,
+      host: info.host,
+      client: state.client
+    })
+
     {:noreply, state}
   end
 
   # handle nick changes
   def handle_info({:nick_changed, old_nick, new_nick}, state) do
     Logger.info("Nick changed from #{old_nick} to #{new_nick}")
+
+    HandlerRegistry.handle_message(:nick_changed, %{
+      old_nick: old_nick,
+      new_nick: new_nick,
+      client: state.client
+    })
+
     {:noreply, state}
   end
 
   # handle users joining channels
   def handle_info({:joined, channel, user}, state) do
     Logger.debug("#{user} joined #{channel}")
+
+    HandlerRegistry.handle_message(:user_joined, %{
+      user: user,
+      channel: channel,
+      client: state.client
+    })
+
     {:noreply, state}
   end
 
   # handle users leaving channels
   def handle_info({:left, channel, user}, state) do
     Logger.debug("#{user} left #{channel}")
+
+    HandlerRegistry.handle_message(:user_left, %{
+      user: user,
+      channel: channel,
+      client: state.client
+    })
+
     {:noreply, state}
   end
 
   # handle users quitting the server
   def handle_info({:quit, user, message}, state) do
     Logger.debug("#{user} quit: #{message}")
+
+    HandlerRegistry.handle_message(:user_quit, %{
+      user: user,
+      message: message,
+      client: state.client
+    })
+
     {:noreply, state}
   end
 
