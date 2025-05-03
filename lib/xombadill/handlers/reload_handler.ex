33dcc8f4d743f083d18265adc13f33bc76702d6b
Jared Miller <jared@smell.flowers>
2025-05-03 18:15:36 -0400

    Ensure reload only reacts to !reload with no args

--- lib/xombadill/handlers/reload_handler.ex
+++ lib/xombadill/handlers/reload_handler.ex
@@ -6,11 +6,6 @@ defmodule Xombadill.Handlers.ReloadHandler do
   @impl true
   def handle_message(:channel_message, %{text: text, channel: channel, client: client} = _msg) do
     cond do
-      String.starts_with?(text, "!reload ") ->
-        module_name = String.trim_leading(text, "!reload ") |> String.trim()
-        # Use Task.start to avoid blocking the handler
-        Task.start(fn -> ReloadCoordinator.reload_module(module_name, channel, client) end)
-
       text == "!reload" ->
         Task.start(fn -> ReloadCoordinator.reload_all_handlers(channel, client) end)
 
