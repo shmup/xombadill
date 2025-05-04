1f2c5c3b3d05b9c63495ce64848fb55af328187c
Jared Miller <jared@smell.flowers>
2025-05-04 00:14:16 -0400

    Rename dev.exs to config.exs so it's picked up

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ config :logger,
   level: :info
 
 config :logger, :console,
-  format: "$time $metadata[$level] $message",
+  format: "$time $metadata[$level] $message\n",
   metadata: [:user_id],
   colors: [enabled: true],
   device: :user
